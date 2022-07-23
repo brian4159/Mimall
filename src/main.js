0f8567a0248fe777e13dc158e79ab59f15bcd665
brian4159 <88099867+brian4159@users.noreply.github.com>
2022-07-23 17:33:41 +0800

    interface defaults intercept

--- src/main.js
+++ src/main.js
@@ -1,7 +1,24 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import axios from "axios"
+import VueAxios from "vue-axios";
 
+//根据前端的跨域方式做调整
+axios.defaults.baseURL = '/api';
+axios.defaults.timeout = '8000';
+//接口错误拦截
+axios.interceptors.response.use(function(){
+  let res = response.data;
+  if(res.status == 0){
+    return res.data;
+  }else if(res.status == 10){
+    window.Location.href = '/#/login';
+  }else{
+    alert(res.msg)
+  }
+});
+Vue.use(VueAxios,axios);
 Vue.config.productionTip = false;
 
 new Vue({
